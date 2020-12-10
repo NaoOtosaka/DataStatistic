c629cfb2d8af5f104d57cd4daee0bb06c96b85a2
NaoOtosaka <1049798454@qq.com>
2020-12-10 21:50:34 +0800

    开发人员侧接口调整 需求调整

--- api/bin/setup.py
+++ api/bin/setup.py
@@ -1,10 +1,12 @@
 from api.lib.project import setup as project_setup
 from api.lib.tester import setup as tester_setup
+from api.lib.developer import setup as developer_setup
 
 
 def main():
     project_setup()
     tester_setup()
+    developer_setup()
 
 
 if __name__ == '__main__':
